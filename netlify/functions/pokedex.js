d1034c689638dc6ed6b47370209bb12eabc84cc8
Catalan-Nick <catalan1222@yahoo.com>
2022-09-21 14:47:39 -0500

    removed 'kanto'

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 
 exports.handler = async function () {
-    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex'
 
     const response = await fetch(POKE_API)
     const data = await response.json()
