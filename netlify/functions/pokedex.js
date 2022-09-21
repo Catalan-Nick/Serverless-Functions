d16e5236609c18fd9ce8f3d9f7f762ca9840daf5
Catalan-Nick <catalan1222@yahoo.com>
2022-09-21 15:04:20 -0500

    switched to pokemon_entries

--- netlify/functions/pokedex.js
+++ netlify/functions/pokedex.js
@@ -1,13 +1,15 @@
 import fetch from 'node-fetch';
 
 exports.handler = async function () {
-    const POKE_API = 'https://pokeapi.co/api/v2/pokedex'
+    const POKE_API = 'https://pokeapi.co/api/v2/pokedex/kanto'
 
     const response = await fetch(POKE_API)
     const data = await response.json()
     console.log(data)
     return{
         statusCode:200,
-        body: JSON.stringify(data)
+        body: JSON.stringify({
+            pokemon: data.pokemon_entries
+        })
     }
 }
