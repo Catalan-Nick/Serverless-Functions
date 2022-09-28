df33e58d1e267bf1b892e6be96e582f19e6567f4
Catalan-Nick <catalan1222@yahoo.com>
2022-09-27 22:38:48 -0500

    Three regions loading cards

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -2,7 +2,7 @@ exports.handler = async function () {
     return{
         statusCode: 200,
         body: JSON.stringify({
-            message: 'Hello there'
+            message: 'I wanna be the very best like no one ever was. Every challenge along the way with courage I will face. I will battle every day to claim my rightful place. Come with me, the time is right There\'s no better team. Arm in arm, we\'ll win the fight. It\'s always been our dream'
         }),
     }
 }
--- public/index.js
+++ public/index.js
@@ -1,19 +1,60 @@
 
       document.addEventListener('DOMContentLoaded', () => {
-        const fetchBtn = document.getElementById('fetch-btn');
+        const homeBtn = document.getElementById('home-btn');
+        
         const fetchKantoBtn = document.getElementById('fetch-kanto-btn');
         const fetchHoennBtn = document.getElementById('fetch-hoenn-btn');
+        const fetchJohtoBtn = document.getElementById('fetch-johto-btn');
         const responseText = document.getElementById('response-output');
         
+       async function getPokeData(name){
+        const url = "https://pokeapi.co/api/v2/pokemon/" + name
+        const response = await fetch(url)
+        const data = await response.json();
+        // console.log(data.sprites.front_default)
 
-        function display(object){
+         return data;
+        }
+
+        async function display(object){
           const responseName = JSON.stringify(object.pokemon_species.name);
+          const pokeName = responseName.replaceAll('"', '')
           const responseUrl = JSON.stringify(object.pokemon_species.url);
-
+          
+          const pokeData = await getPokeData(pokeName);
+          console.log(pokeData)
+          
           const card = document.createElement('div');
+          const cardImg = document.createElement('img');
           const cardBody = document.createElement('div');
-          card.classList.add("card", "bg-primary")
-          card.innerText = responseName
+          const cardTitle = document.createElement('h5');
+          const cardText = document.createElement('p');
+          const cardLink = document.createElement('a');
+
+          card.classList.add("card", "card-color", "m-2", "col-sm-3", "bg-dark");
+          cardImg.classList.add("card-img-top");
+          cardBody.classList.add("card-body", "bg-danger");
+          cardTitle.classList.add("card-title", "text-outline");
+          cardText.classList.add("card-text");
+          cardLink.classList.add("btn", "btn-primary");
+
+          cardImg.src = pokeData.sprites.front_default
+
+
+          card.append(cardImg);
+          card.append(cardBody);
+          cardBody.append(cardTitle);
+          cardBody.append(cardText);
+          cardBody.append(cardLink);
+        
+
+          cardTitle.innerText = pokeName
+
+            
+
+
+
+
           responseText.append(card)
           
         }
@@ -28,7 +69,7 @@
 
           const pokemon = JSON.stringify(response.pokemon)
           const obj = JSON.parse(pokemon)
-          console.log(obj[0])
+          responseText.innerText = ""
           obj.forEach(display);
     
         })
@@ -44,13 +85,30 @@
           
           const pokemon = JSON.stringify(response.pokemon)
           const obj = JSON.parse(pokemon)
-          console.log(obj[0])
+          responseText.innerText = ""
           obj.forEach(display)
         })
+        fetchJohtoBtn.addEventListener('click', async () => {
+            const response = await fetch('/.netlify/functions/pokedex', {
+              method: 'POST',
+              body: JSON.stringify({
+                region: 'original-johto'
+              })
+            }).then(response => response.json())
+  
+            const pokemon = JSON.stringify(response.pokemon)
+            const obj = JSON.parse(pokemon)
+            responseText.innerText = ""
+            obj.forEach(display);
+      
+          })
 
-        fetchBtn.addEventListener('click', async () => {
+        homeBtn.addEventListener('click', async () => {
           const response = await fetch('/.netlify/functions/hello-world')
           .then(response => response.json())
           responseText.innerText = JSON.stringify(response.message);
+          responseText.classList.add("text-xl-center", "text-outline")
         })
+        //home on page load
+        homeBtn.click();
       })
