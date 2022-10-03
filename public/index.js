3a557fefa139d2bada869911df51d25bb52ee607
Catalan-Nick <catalan1222@yahoo.com>
2022-10-03 15:00:30 -0500

    fetch fixes

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'
+
 const fetch = require("node-fetch");
 document.addEventListener('DOMContentLoaded', () => {
         const homeBtn = document.getElementById('home-btn');
