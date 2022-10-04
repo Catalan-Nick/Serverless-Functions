19a2e0387f1494166105148bb9a3280735945178
Catalan-Nick <catalan1222@yahoo.com>
2022-10-03 22:26:22 -0500

    api cal

--- netlify/functions/getPokeData.js
+++ netlify/functions/getPokeData.js
@@ -1,18 +1,22 @@
 import fetch from 'node-fetch'
-import Pokedex from 'pokedex-promise-v2';
-const P = new Pokedex();
+// import Pokedex from 'pokedex-promise-v2';
+// const P = new Pokedex();
 
 
 exports.handler = async (event, context) => {
     const eventBody = JSON.parse(event.body);
     const POKE_API = 'https://pokeapi.co/api/v2/pokedex/' + eventBody.name
 
-    const response = await P.getPokemonByName(eventBody.name)
-        console.log(response);
+
+    const response = await fetch(POKE_API)
+    const data = await response.json()
+
+    // const response = await P.getPokemonByName(eventBody.name)
+    //     console.log(response);
     return{
         statusCode: 200,
         body: JSON.stringify({
-            pokemon: POKE_API
+            pokemon: data
         }),
     }
 }
